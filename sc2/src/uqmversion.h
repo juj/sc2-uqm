dd4c3e433e49e126239a7c02f8f6b695aceb5f36
Michael Martin <mcmartin@gmail.com>
2019-08-17 20:48:44 -0700

    Update version to 0.7.1 alpha

--- sc2/src/uqmversion.h
+++ sc2/src/uqmversion.h
@@ -21,9 +21,9 @@
 #define UQM_MAJOR_VERSION_S  "0"
 #define UQM_MINOR_VERSION     7
 #define UQM_MINOR_VERSION_S  "7"
-#define UQM_PATCH_VERSION     0
-#define UQM_PATCH_VERSION_S  "0"
-#define UQM_EXTRA_VERSION    ""
+#define UQM_PATCH_VERSION     1
+#define UQM_PATCH_VERSION_S  "1"
+#define UQM_EXTRA_VERSION    "a"
 /* The final version is interpreted as:
  * printf ("%d.%d.%d%s", UQM_MAJOR_VERSION, UQM_MINOR_VERSION,
  * 		UQM_PATCH_VERSION, UQM_EXTRA_VERSION);
