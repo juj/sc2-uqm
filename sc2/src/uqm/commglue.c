ca9174d31757d36218c62a284145ebb3c81798da
Michael Martin <mcmartin@gmail.com>
2020-06-09 17:18:36 -0700

    Fix strncpy warning by making it more obviously dangerous
    The function as a whole is boundschecked after the fact, but as its
    comments state, this function really needs its API changed to carry
    buffer lengths in from the outside.

--- sc2/src/uqm/commglue.c
+++ sc2/src/uqm/commglue.c
@@ -284,7 +284,7 @@ construct_response (UNICODE *buf, int R /* promoted from RESPONSE_REF */, ...)
 		if (name)
 		{
 			len = strlen (name);
-			strncpy (buf, name, len);
+			strcpy (buf, name);
 			buf += len;
 			
 			/*
